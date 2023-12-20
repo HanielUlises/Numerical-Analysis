71027f39eaae761b6ebada00a6c0b3a00c6d37bd
Haniel Ulises <hanielulises2003@gmail.com>
2023-12-20 11:03:59 -0600

    Jacobi method

---
+++ Linear Algebra/Linear Equation Methods/Jacobi Method/Jacobi Method/jacobi.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+std::vector<double> jacobi(const std::vector<std::vector<double>>& A, const std::vector<double>& b, std::vector<double> x, int max_iter) {
+    int n = A.size();
+    std::vector<double> prev_x = x;
+
+    for (int iter = 0; iter < max_iter; ++iter) {
+        for (int i = 0; i < n; ++i) {
+            double sum = 0.0;
+            for (int j = 0; j < n; ++j) {
+                if (i != j) {
+                    sum += A[i][j] * prev_x[j];
+                }
+            }
+            x[i] = (b[i] - sum) / A[i][i];
+        }
+        prev_x = x; // Update the previous iteration values
+    }
+
+    return x;
+}
+
+int main() {
+    std::vector<std::vector<double>> A = {
+        {2, 1, -1},
+        {-1, 2, 2},
+        {3, 2, -3}
+    };
+    std::vector<double> b = {3, 8, 2};
+    std::vector<double> x0 = {2, 2, 2};
+    int max_iter = 25;
+
+    std::vector<double> x = jacobi(A, b, x0, max_iter);
+
+    for (int i = 0; i < x.size(); ++i) {
+        std::cout << "x" << i+1 << " = " << x[i] << std::endl;
+    }
+
+    return 0;
+}
--- Linear Algebra/Linear Equation Methods/Jacobi Method/Jacobi Method/jacobi2.cpp
+++ Linear Algebra/Linear Equation Methods/Jacobi Method/Jacobi Method/jacobi2.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <iomanip>
 
 std::vector<double> jacobi(const std::vector<std::vector<double>>& A, const std::vector<double>& b, std::vector<double> x, int max_iter) {
     int n = A.size();
-    std::vector<double> prev_x = x;
-
+    std::vector<double> prev_x(n, 0.0);
+    std::vector<double> new_x(n, 0.0);
+    
     for (int iter = 0; iter < max_iter; ++iter) {
+        for (int i = 0; i < n; ++i) {
+            prev_x[i] = x[i]; // Store the old values
+        }
+
         for (int i = 0; i < n; ++i) {
             double sum = 0.0;
             for (int j = 0; j < n; ++j) {
-                // Diagonal matrix
                 if (i != j) {
                     sum += A[i][j] * prev_x[j];
                 }
             }
-            x[i] = (b[i] - sum) / A[i][i];
+            new_x[i] = (b[i] - sum) / A[i][i];
         }
-        prev_x = x; // Update the previous iteration values
+        
+        // Update the iteration values
+        x = new_x;
     }
 
     return x;
@@ -25,18 +32,19 @@ std::vector<double> jacobi(const std::vector<std::vector<double>>& A, const std:
 
 int main() {
     std::vector<std::vector<double>> A = {
-        {2, 1, -1},
-        {-1, 2, 2},
-        {3, 2, -3}
+        {2, 1, -1, 1},
+        {-1, 2, 2, -1},
+        {3, 2, -3, 4},
+        {1, -1, 1, -2}
     };
-    std::vector<double> b = {3, 8, 2};
-    std::vector<double> x0 = {2, 2, 2};
+    std::vector<double> b = {3, 8, 2, -1};
+    std::vector<double> x0 = {3, 3, 3, 3};
     int max_iter = 25;
 
     std::vector<double> x = jacobi(A, b, x0, max_iter);
 
     for (int i = 0; i < x.size(); ++i) {
-        std::cout << "x" << i+1 << " = " << x[i] << std::endl;
+        std::cout << "x" << i+1 << " = " << std::setprecision(10) << x[i] << std::endl;
     }
 
     return 0;
